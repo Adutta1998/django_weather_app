c26369b0858322aeb179ba2e92f996c37d72c2ef
Avinaba Dutta <duttaavinaba98@gmail.com>
2018-12-07 03:08:07 +0530

    new pin/unpin methode added

--- weather/app/urls.py
+++ weather/app/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index,name='weather'),
+    path('pin/<city>', views.pin,name='pin'),
+    path('unpin/<city>', views.unpin,name='unpin'),
 ]
--- weather/app/views.py
+++ weather/app/views.py
@@ -1,12 +1,31 @@
-from django.shortcuts import render,HttpResponse
+from django.shortcuts import render,HttpResponse,redirect
 from urllib import request
 import json
+from .models import WeatherData
 
 # Create your views here.
 def index(req):
     if req.GET.get('city'):
+        d={}
         raw_data = request.urlopen('http://api.openweathermap.org/data/2.5/weather?q={0}&APPID=fe2658af87ff250fe0a69fc5077a954a&units=metric'.format(req.GET.get('city')))
         data = json.load(raw_data)
+        d["testdata"] = data
+        print(d)
         return render(req,'app/weather.html',{"weather_data":data})
     else:
-        return render(req,'app/weather.html')
+        cities = WeatherData.objects.all()
+        data = []
+        for city in cities:
+            raw_data = request.urlopen('http://api.openweathermap.org/data/2.5/weather?q={0}&APPID=fe2658af87ff250fe0a69fc5077a954a&units=metric'.format(city))
+            data.append(json.load(raw_data))
+        #print(data)
+        return render(req,'app/weather.html',{'data':data})
+
+def pin(request,city):
+    data = WeatherData(city=city)
+    data.save()
+    return redirect(index)
+
+def unpin(request,city):
+    WeatherData.objects.get(city=city).delete()
+    return redirect(index)
